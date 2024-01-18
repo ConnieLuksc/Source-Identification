8beb5175d50c6130ebf64e179469d2edb3c41e57
ConnieLuksc <1155142806@link.cuhk.edu.hk>
2024-01-18 12:46:51 -0600

    Add search bar

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,41 @@
-import Head from 'next/head';
-
+import Link from "next/link"
 export default function Header() {
   return (
     <>
-      <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet" />
-      </Head>
-      <h1 className="font-kaushan">EchoExpert</h1>
+      <header
+        className="top-0 sticky z-20 w-full flex flex-row animate-in fade-in slide-in-from-top-4 duration-1000 ease-in-out"
+      >
+        <>
+        <Link
+          className="items-left flex flex-row justify-center pt-4"
+          href="/"
+        >
+          <img
+            src="/Logo.png"  
+            alt="EchoExpert Logo"
+            width="200"  
+            height="200"
+          />
+        </Link>
+
+        
+        <button
+          className="fixed top-0 right-0 m-4 flex flex-row flex-shrink-0 items-center justify-center rounded-lg truncate transition duration-200 ease-out disabled:pointer-events-auto disabled:opacity-50 bg-white shadow ring-1 ring-gray-200 hover:bg-gray-50 px-2.5 sm:px-2 py-2 h-9 sm:h-8 sm:text-sm rounded-lg font-medium input-focus-ring select-none"
+          type="button"
+          id="account-button"
+          aria-haspopup="menu"
+          aria-expanded="false"
+        >
+          <span className="material-symbols-outlined">
+          person
+          </span>
+        </button>
+        </>
+
+        
+      </header>
+
+
     </>
   );
 }
