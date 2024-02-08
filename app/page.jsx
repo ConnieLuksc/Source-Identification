d691361febf7c8ce9a7fb731494c35063bc33f57
Hunterxia <hunterxia@outlook.com>
2024-02-08 12:05:44 -0600

    Add expert details to expert page

--- app/page.jsx
+++ app/page.jsx
@@ -3,18 +3,22 @@ import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import ExpertCard from "./components/ExpertCard";
 import useEventStore from "./store/eventStore";
-import experts from './data/experts_data.json';
+import experts from "./data/experts_data.json";
 
 export default function Home() {
   const searchQuery = useEventStore((state) => state.searchQuery);
   const setSearchQuery = useEventStore((state) => state.setSearchQuery);
 
-
   const filteredExperts = experts.filter((expert) => {
     return (
       expert.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      expert.email.toLowerCase().includes(searchQuery.toLowerCase()) ||
       expert.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      expert.school.toLowerCase().includes(searchQuery.toLowerCase()) ||
       expert.fields.some((tag) =>
+        tag.toLowerCase().includes(searchQuery.toLowerCase())
+      ) ||
+      expert.focus_areas.some((tag) =>
         tag.toLowerCase().includes(searchQuery.toLowerCase())
       )
     );
@@ -34,4 +38,3 @@ export default function Home() {
     </div>
   );
 }
-
