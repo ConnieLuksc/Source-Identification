9eedbfd275665adabbff6dbf450bb64b431c42c7
ConnieLuksc <1155142806@link.cuhk.edu.hk>
2024-02-08 12:14:01 -0600

    Change header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,11 +4,11 @@ import AccountButton from "./Login";
 
 export default function Header() {
   return (
-    <header className="top-0 sticky z-20 w-full flex flex-row animate-in fade-in slide-in-from-top-4 duration-1000 ease-in-out">
+    <header className="top-0 sticky z-20 w-full flex flex-row animate-in fade-in slide-in-from-top-4 duration-1000 ease-in-out bg-white">
       <Link className="items-left flex flex-row justify-center pt-4" href="/">
         <Image src="/Logo.png" alt="EchoExpert Logo" width={200} height={200} />
       </Link>
-      <AccountButton></AccountButton>
+      <AccountButton/>
     </header>
   );
 }
