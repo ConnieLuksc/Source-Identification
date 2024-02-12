aa08c64b9523906a06905e53f57db8a8099e95f2
Hunterxia <hunterxia@outlook.com>
2024-02-11 20:10:48 -0600

    Refactor ExpertCard and Header components

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,7 +6,15 @@ export default function Header() {
   return (
     <header className="top-0 sticky z-20 w-full flex flex-row animate-in fade-in slide-in-from-top-4 duration-1000 ease-in-out bg-white">
       <Link className="items-left flex flex-row justify-center pt-4" href="/">
-        <Image src="/Logo.png" alt="EchoExpert Logo" width={200} height={200} />
+        <Image
+          src="/Logo.png"
+          alt="EchoExpert Logo"
+          width={200}
+          height={200}
+          style={{
+            maxWidth: "100%",
+            height: "auto"
+          }} />
       </Link>
       <AccountButton/>
     </header>
